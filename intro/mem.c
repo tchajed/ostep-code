3b326639a4935e66e507967caca88ebb38fb20da
Tej Chajed <chajed@wisc.edu>
2024-01-22 15:48:55 -0600

    Make mem.c match book

--- intro/mem.c
+++ intro/mem.c
@@ -4,15 +4,10 @@
 #include <unistd.h>
 
 int main(int argc, char *argv[]) {
-  if (argc != 2) {
-    fprintf(stderr, "usage: mem <value>\n");
-    exit(1);
-  }
-  int *p;
-  p = malloc(sizeof(int));
+  int *p = malloc(sizeof(int));
   assert(p != NULL);
-  printf("(%d) addr pointed to by p: %p\n", (int)getpid(), p);
-  *p = atoi(argv[1]); // assign value to addr stored in p
+  printf("(%d) addr pointed to by p: %p\n", getpid(), p);
+  *p = 0;
   while (1) {
     Spin(1);
     *p = *p + 1;
