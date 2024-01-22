ba5bb7cbdb3e7f12816fe2823eab6c0405d0432a
Tej Chajed <chajed@wisc.edu>
2024-01-22 16:03:51 -0600

    Make io.c match book
    Simplifies to eliminate the string processing parts

--- intro/io.c
+++ intro/io.c
@@ -1,7 +1,5 @@
 #include <assert.h>
 #include <fcntl.h>
-#include <stdio.h>
-#include <string.h>
 #include <sys/stat.h>
 #include <sys/types.h>
 #include <unistd.h>
@@ -9,11 +7,8 @@
 int main(int argc, char *argv[]) {
   int fd = open("/tmp/file", O_WRONLY | O_CREAT | O_TRUNC, S_IRUSR | S_IWUSR);
   assert(fd >= 0);
-  char buffer[20];
-  sprintf(buffer, "hello world\n");
-  int rc = write(fd, buffer, strlen(buffer));
-  assert(rc == (strlen(buffer)));
-  fsync(fd);
+  int rc = write(fd, "hello world\n", 13);
+  assert(rc == 13);
   close(fd);
   return 0;
 }
--- intro/threads.c
+++ intro/threads.c
@@ -1,4 +1,3 @@
-#include "common.h"
 #include "common_threads.h"
 #include <stdio.h>
 #include <stdlib.h>
